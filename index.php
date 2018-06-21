c1a5ed4ed585192e2e497bb7797b8d956b84f303
Allex Lima <allexlima97@gmail.com>
2018-06-20 22:24:32 -0400

    testing model/i_colab methods

--- index.php
+++ index.php
@@ -1,27 +1,29 @@
 <?php
 
-  error_reporting(E_ALL);
-  ini_set('display_errors', 1);
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
 
-  require_once 'src/model/connect.php';
-  require_once 'src/model/i_colab.php';
+    require_once 'src/model/connect.php';
+    require_once 'src/model/i_colab.php';
 
-  $a = new Model_icolab($pdo);
+    $a = new ModelColaboradores($pdo);
 
-  switch(@$_GET['pg']){
-    case "login":
-      echo $a->login($_POST['email'], $_POST['password']);
-      break;
-    case "cadastro":
-      echo $a->create($_POST['nome'], $_POST['email'], $_POST['password']);
-      break;
-    case "is_admin":
-      echo $a->is_admin($_POST['email']);
-      break;
-    default:
-      echo "Escolha um recurso!";
-      break;
-  }
-
+    switch(@$_GET['pg']){
+        case "login":
+            echo $a->login($_POST['email'], $_POST['password']);
+            break;
+        case "create":
+            echo $a->create($_POST['nome'], $_POST['email'], $_POST['password']);
+            break;
+        case "is_admin":
+            echo $a->is_admin($_POST['email']);
+            break;
+        case "update":
+            echo $a->update($_POST['id'], $_POST['nome'], $_POST['email'], $_POST['password'], $_POST['funcao']);
+            break;
+        default:
+            echo "Escolha um recurso!";
+            break;
+    }
 
 ?>
