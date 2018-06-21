cfa44f770899f10d5085ea8d91f79883182ae215
Allex Lima <allexlima97@gmail.com>
2018-06-20 22:08:36 -0400

    Finishing model for 'Colaboradores' schema

--- src/model/i_colab.php
+++ src/model/i_colab.php
@@ -1,38 +1,70 @@
 <?php
 
-class Model_icolab{
+class ModelColaboradores{
 
-  private $conn;
+    private $conn;
 
-  function __construct($connection){
-    $this->conn = $connection;
-  }
+    function __construct($connection){
+        $this->conn = $connection;
+    }
 
-  public function login($email, $password){
-    $sql = "SELECT colab_id FROM i_colab WHERE colab_email = (?) AND colab_senha = (?)";
-    $query = $this->conn->prepare($sql);
-    $query->execute(array($email, $password));
-    $result = $query->fetch();
-    return (!$result) ? -1 : $result[0];
-  }
+    public function login($email, $password){
+        $sql = "SELECT colab_id FROM i_colab WHERE colab_email = (?) AND colab_senha = (?)";
+        $query = $this->conn->prepare($sql);
+        $query->execute(array($email, $password));
+        $result = $query->fetch();
+        return (!$result) ? -1 : $result[0];
+    }
 
-  public function create($name, $email, $password, $post=Null, $admin='false'){
-    $sql = "INSERT INTO i_colab (colab_nome, colab_email, colab_senha, colab_funcao, colab_supervisor) VALUES ((?), (?), (?), (?), (?)) RETURNING colab_id";
-    $query = $this->conn->prepare($sql);
-    $query->execute(array($name, $email, $password, $post, $admin));
-    $result = $query->fetch();
-    return (!$result) ? -1 : $result[0];
-  }
+    public function create($name, $email, $password, $post=Null, $admin='false'){
+        $sql = "INSERT INTO i_colab (colab_nome, colab_email, colab_senha, colab_funcao, colab_supervisor) VALUES ((?), (?), (?), (?), (?)) RETURNING colab_id";
+        $query = $this->conn->prepare($sql);
+        $query->execute(array($name, $email, $password, $post, $admin));
+        $result = $query->fetch();
+        return (!$result) ? -1 : $result[0];
+    }
 
-  public function is_admin($email){
-    $query = $this->conn->prepare("SELECT colab_id FROM i_colab WHERE colab_email = (?) AND colab_supervisor = (true)");
-    $query->execute(array($email));
-    $query->execute();
-    $result = $query->fetch();
-    return (!$result) ? 0 : 1;
-  }
+    public function is_admin($email){
+        $sql = "SELECT colab_id FROM i_colab WHERE colab_email = (?) AND colab_supervisor = (true)";
+        $query = $this->conn->prepare($sql);
+        $query->execute(array($email));
+        $result = $query->fetch();
+        return (!$result) ? 0 : 1;
+    }
 
+    public function update($id, $name="", $email="", $password="", $post=""){
+        if(empty($name) && empty($email) && empty($password) && empty($post))
+            return -1;
 
+        $sql = "UPDATE i_colab SET ";
+        $sql_add = array();
+        $values = array();
+
+        if(!empty($name)){
+            array_push($sql_add, "colab_nome = (?),");
+            array_push($values, $name);
+        }
+        if(!empty($email)){
+            array_push($sql_add, "colab_email = (?),");
+            array_push($values, $email);
+        }
+        if(!empty($password)){
+            array_push($sql_add, "colab_senha = (?),");
+            array_push($values, $password);
+        }
+        if(!empty($post)){
+            array_push($sql_add, "colab_funcao = (?),");
+            array_push($values, $post);
+        }
+
+        array_push($values, $id);
+
+        $sql .= implode($sql_add)." colab_supervisor = (false) ";
+        $sql .= "WHERE colab_id = (?)";
+
+        $query = $this->conn->prepare($sql);
+        $query->execute($values);
+    }
 
 }
 
